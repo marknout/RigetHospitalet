a8fa32fd1c55977c81fc4d36bb24e623fb3e10c6
LauraBlicher <Lblicher@gmail.com>
2019-04-04 14:16:04 +0200

    bird sprites

---
+++ Sanctuary/Assets/Scripts/DandilionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DandilionScript : MonoBehaviour
+{
+
+    public ParticleSystem dandilionSeeds;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        dandilionSeeds.Play();
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
