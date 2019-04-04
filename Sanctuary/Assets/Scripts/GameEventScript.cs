9a441f61073e5cda7ba8e7debff8e16a6dc96ebd
LauraBlicher <Lblicher@gmail.com>
2019-04-04 10:55:55 +0200

    Water ripple effect

---
+++ Sanctuary/Assets/Scripts/GameEventScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventScript : MonoBehaviour
+{
+    public Camera cam;
+    public LayerMask mask;
+    public GameObject ripple;
+    public float time;
+    public AudioSource aSource;
+    public AudioClip splash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GetComponent<AudioSource> (); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray,out hit, Mathf.Infinity, mask, QueryTriggerInteraction.Ignore))
+            {
+                GameObject newRipple;
+                newRipple = Instantiate(ripple, hit.point, transform.rotation);
+                aSource.PlayOneShot(splash);
+                Destroy(newRipple, time);
+            }
+        }
+    }
+}
