6333889cff93e049a9bab01562fb308313f850b5
LauraBlicher <Lblicher@gmail.com>
2019-04-17 12:00:29 +0200

    houses, seagull, smoke, dandilion fixes

---
+++ Sanctuary/Assets/Scripts/SwitchAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAnimation : MonoBehaviour { 
+      public float delay = 1f;
+      private Animator anim;
+      public AudioSource aSource;
+      public AudioClip soundEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        aSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        StartCoroutine(ResetAnim());
+        aSource.PlayOneShot(soundEffect);
+
+    }
+
+    IEnumerator ResetAnim()
+    {
+        anim.SetBool("isClicked", true);
+        yield return new WaitForSeconds(delay);
+        anim.SetBool("isClicked", false);
+    }
+ }
---
+++ Sanctuary/Assets/Scripts/SwitchSprite.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSprite : MonoBehaviour
+{
+    public bool visualOn;
+    public AudioSource aSource;
+    public AudioClip soundEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        visualOn = false;
+        aSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (visualOn)
+        //{
+        //    GetComponent<SpriteRenderer>().enabled = false;
+        //
+        //}
+        //if (!visualOn)
+        //{
+        //    GetComponent<SpriteRenderer>().enabled = true;
+        //
+        //}
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.enabled = !visualOn;
+    }
+
+    void OnMouseDown()
+    {
+        if (StaticPlayer.playerState == StaticPlayer.PlayerState.noState)
+        {
+            visualOn = !visualOn;
+            aSource.PlayOneShot(soundEffect);
+        }
+    }
+                   
+}
